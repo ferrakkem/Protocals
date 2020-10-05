48f8e798c57a5d1b016b7b96cce8c165bae8383a
Ferrakkem Bhuiyan <ferrakkem@gmail.com>
2020-10-05 15:25:11 -0400

    code update

--- Protocals/main.swift
+++ Protocals/main.swift
@@ -5,7 +5,55 @@
 //  Created by Ferrakkem Bhuiyan on 2020-10-05.
 //
 
-import Foundation
+protocol CanFly {
+    func fly()
+}
 
-print("Hello, World!")
+
+class Bird{
+    
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("The Bird makes a new bird in a shell.")
+        }
+    }
+    
+    /*
+    func fly(){
+        print("The bird its wings and lifts off into the sky")
+    }
+    */
+}
+
+class Eagle: Bird, CanFly{
+    func fly() {
+        print("The Eagle its wings and lifts off into the sky")
+    }
+
+    func soar(){
+        print("Eagle can soar...")
+    }
+}
+
+
+class Penguin: Bird {
+    func swim(){
+        print("Penguin can swim")
+    }
+}
+
+struct FlyingMusem {
+    
+}
+
+struct Airple: CanFly {
+    func fly() {
+        print("The Airple uses its engine to  lift off into the sky")
+    }
+
+}
+
+let myEagle = Eagle()
 
